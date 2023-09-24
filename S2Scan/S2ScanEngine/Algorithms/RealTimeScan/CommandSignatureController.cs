90d5f4b48e48ac20a9a97ef248eb4c4fb90a7009
shells <singletonshells@gmail.com>
2023-09-25 00:11:37 +0200

    Get A List Of Known Malicious Command Line Signatures, And Malicoius Task Manager processes

---
+++ S2Scan/S2ScanEngine/Algorithms/RealTimeScan/CommandSignatureController.cs
@@ -0,0 +1,39 @@
+﻿using FireSharp.Response;
+using Newtonsoft.Json;
+using S2ScanEngine.data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2ScanEngine.Algorithms.RealTimeScan
+{
+    public class CommandSignatureController
+    {
+        S2ScanDbConnection conn = new S2ScanDbConnection();
+        public List<CommandModel> commandSignatureList = new List<CommandModel>();
+
+        public CommandSignatureController()
+        {
+            commandSignatureList = LoadData();
+        }
+
+
+        // return known command signatures
+        public List<CommandModel> LoadData()
+        {
+            try
+            {
+                FirebaseResponse al = conn.client.Get("commandSignatures");
+                List<CommandModel> commandSignatures = JsonConvert.DeserializeObject<List<CommandModel>>(al.Body.ToString());
+                return commandSignatures;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Encountered An Error Fetching Data");
+                return null;
+            }
+        }
+    }
+}
---
+++ S2Scan/S2ScanEngine/Algorithms/RealTimeScan/ProcessSignatureController.cs
@@ -0,0 +1,37 @@
+﻿using FireSharp.Response;
+using Newtonsoft.Json;
+using S2ScanEngine.data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2ScanEngine.Algorithms.RealTimeScan
+{
+    public class ProcessSignatureController
+    {
+        S2ScanDbConnection conn = new S2ScanDbConnection();
+        public List<ProcessModel> processSignatureList = new List<ProcessModel>();
+
+        public ProcessSignatureController()
+        {
+            
+        }
+
+        public List<ProcessModel> LoadData()
+        {
+            try
+            {
+                FirebaseResponse al = conn.client.Get("processSignatures");
+                List<ProcessModel> processSignatures = JsonConvert.DeserializeObject<List<ProcessModel>>(al.Body.ToString());
+                return processSignatures;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Encountered An Error Fetching Data");
+                return null;
+            }
+        }
+    }
+}
